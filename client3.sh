d2ba8d5b6c6f2ba8ed7e0345667e405932172cbf
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-07-13 00:11:55 -0400

    test with/without priming

--- client3.sh
+++
@@ -1,5 +0,0 @@
-curl --no-buffer -s -S -X                         \
-  POST "http://127.0.0.1:5555/query"  \
-  -H 'Content-Type: application/json' \
-  -d '{"messages": [{"role": "system", "content": "You are a helpful, respectful and honest assistant"}, {"role": "user", "content": "What is the difference between concurrency and parallelism"}], "complete": true}'
-
---
+++ test_duo.sh
@@ -0,0 +1,7 @@
+sleep 10
+
+curl --no-buffer -s -S -X                         \
+  POST "http://127.0.0.1:5555/query"  \
+  -H 'Content-Type: application/json' \
+  -d @data/in0.json
+
